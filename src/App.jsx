0aa181dafc640b84cc7c5638ff7786f54e5db751
Zaheen Rakib <rakibzaheen@gmail.com>
2024-03-13 12:41:34 +0600

    Recap Six Concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Posts from './Posts'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>React Core Concepts Recap</h1>
+      <h3>React Core Concepts Recap</h3>
+      <ol>
+        <li>Components</li>
+        <li>JSX</li>
+        <li>Props</li>
+        <li>Event Handler</li>
+        <li>State</li>
+        <li>Load Data</li>
+      </ol>
+      <hr />
+      <Posts></Posts>
     </>
   )
 }
